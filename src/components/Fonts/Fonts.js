b67d181a2cc88abf6e92111e517f5891dd0cba9c
osequi <cs@osequi.com>
2020-09-11 14:51:39 +0300

    The Fonts component is optimized

--- src/components/Fonts/Fonts.js
+++ src/components/Fonts/Fonts.js
@@ -7,17 +7,7 @@ import Head from "next/head";
 const Fonts = (props) => {
   return (
     <Head>
-      <style>
-        {`
-		@font-face {
-		font-family: "BlackMambaTrial-Venom-WebTrial";
-		font-style: normal;
-		font-weight: normal;
-		src: local("/fonts/black-mamba/BlackMambaTrial-Venom-WebTrial"),
-		  url("/fonts/black-mamba/BlackMambaTrial-Venom-WebTrial.woff") format("woff");
-	  }
-`}
-      </style>
+      <style>{`@import '/fonts/black-mamba/black-mamba.css'`}</style>
     </Head>
   );
 };
